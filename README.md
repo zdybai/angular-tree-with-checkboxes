5f8d6df7e669841c2e5696c6a8719660e6aee72d
zdybai <zdybai93@gmail.com>
2022-07-26 16:18:25 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-8aujbn-vmlwjd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-8aujbn-vmlwjd)
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MaterialExampleModule} from '../material.module';
+import {TreeChecklistExample} from './tree-checklist-example';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  declarations: [TreeChecklistExample],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MaterialExampleModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [TreeChecklistExample],
+})
+export class AppModule {}
